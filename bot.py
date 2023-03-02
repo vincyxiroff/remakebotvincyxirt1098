2dc9917c90fc46570ae513521a860ecb993720c9
vincyxiroff <88449249+vincyxiroff@users.noreply.github.com>
2023-03-02 20:24:57 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -403,6 +403,14 @@ async def on_ready():
     print('Ready to support ✅')
 
 
+@bot.event
+async def on_message(message):
+    bad_words = ["fuck", "arschloch", "https://discord.gg", "http://disord.gg", "discord.gg", "fick", "arsch", "Arschgesicht", "arschgesicht", "Arschloch", "Asshole", "asshole", "Fotze", "fotze", "Miststück", "miststück", "Bitch", "bitch", "Schlampe", "schlampe", "Sheisse", "sheisse", "Shit", "shit", "Fick", "huren", "Verpiss", "verpiss", "masturbiert", "Idiot", "idiot", "depp", "Depp", "Dumm", "dumm", "jude", "Bastard", "bastard", "Wichser", "wichser", "wixxer", "Wixxer", "Hurensohn" "Wixer", "Pisser", "Arschgesicht", "huso", "hure", "Hure", "verreck" "Verreck", "fehlgeburt", "Fehlgeburt", "ficken", "adhs", "ADHS", "Btch", "faggot", "fck", "f4ck", "nigga", "Nutted", "flaschengeburt", "penis", "pusse", "pusse", "pussy", "pussys", "nigger", "kacke", "fuucker"]
+    for word in bad_words:
+        if message.content.count(word) > 0:
+            await message.channel.purge(limit=1)
+            await message.channel.send(f"Stai usando parole non consentite, perfavore smettila! {message.author.mention}")
+
 
 
 
